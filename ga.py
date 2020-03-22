c3360961b1d8aa1b61aea0616e6e0509c3e8262b
TimmyOVO <timmyovo@qq.com>
2020-03-22 14:46:11 +0800

    init

--- ga.py
+++ ga.py
@@ -1,20 +1,94 @@
-import matplotlib.pyplot as plt
-import numpy
-
-
 # 求解the_function在[0,15]的最大值
 
 # Generate some individual with random genetic
 # Calculate individual's fitness and pick some individual
 # Crossover their genetic using specific rule(s)
 # Make their child mutation
-# Repeat step 1 until result meet the stop conditions
+# Algorithm should be terminates if the population has converged
 
-def the_function(z):
-    return z + 10 * numpy.sin(5 * z) + 7 * numpy.cos(4 * z)
+# def the_function(z):
+#     return math.sin(z)
+#
+
+# 1 bit for the sign of number ,0 positive 1 negative
+# 8 bits for the exponent
+# 23 bits for the mantissa
+
+import math
+import random
 
 
-x = numpy.linspace(0, 15)
-y = the_function(x)
-plt.plot(x, y)
-plt.show()
+class GA:
+    PopulationSize = 1000
+    GeneSize = 20
+
+    def __init__(self):
+        self.Population = Population(self.PopulationSize, self.GeneSize)
+        pass
+
+    def start(self):
+        for i in range(0xFFFFFFF):
+            self.Population.selection()
+            self.Population.crossover()
+            self.Population.mutation()
+            print('Generation ', i, ' fitness ', self.Population.Individuals[0].fitness(), ' input ', self.Population.Individuals[0].value())
+        print(self.Population.Individuals)
+
+
+class Population:
+    Individuals = []
+    CrossoverRatio = 0.6
+    MutationRatio = 0.2
+
+    def __init__(self, population_size, gene_size):
+        self.Individuals = []
+        for _ in range(population_size):
+            self.Individuals.append(Individual(gene_size))
+
+    def selection(self):
+        survived_individuals = []
+        for i in self.Individuals:
+            v = random.randrange(0, 100)
+            if v <= (i.fitness() * 100):
+                survived_individuals.append(i)
+        survived_individuals.sort(key=lambda z: z.fitness())
+        self.Individuals = survived_individuals
+
+    def crossover(self):
+        for _ in range(int(len(self.Individuals) * self.CrossoverRatio)):
+            p1 = random.choice(self.Individuals)
+            p2 = random.choice(self.Individuals)
+            self.Individuals.append(p1.crossover(p2))
+
+    def mutation(self):
+        for _ in range(int(len(self.Individuals) * self.MutationRatio)):
+            target = random.choice(self.Individuals)
+            for _ in range(int(len(target.Genes) / 5)):
+                target.Genes[random.randrange(0, len(target.Genes))] = random.randrange(0, 2)
+
+
+class Individual:
+    Genes = []
+
+    def __init__(self, gene_size):
+        self.Genes = []
+        for _ in range(gene_size):
+            self.Genes.append(random.randrange(0, 2))
+
+    def crossover(self, p2):
+        iin = Individual(len(self.Genes))
+        cut_point = random.randrange(0, len(self.Genes))
+        iin.Genes[cut_point:] = self.Genes[cut_point:]
+        iin.Genes[:cut_point] = p2.Genes[:cut_point]
+        return iin
+
+    def value(self):
+        temp = [str(iz) for iz in self.Genes]
+        v = int('0b' + ''.join(temp), 2)
+        return v
+
+    def fitness(self):
+        return math.sin(self.value())
+
+
+GA().start()
